80e8bbf66be93a4c5eed37ccfe46618bbae41aa4
yanqi <hi@yanqi.me>
2025-06-22 18:28:23 +0800

    chore: adjust the content margin

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,10 +17,10 @@ onMounted(loadWords)
 </script>
 
 <template>
-  <div mt-12 p4>
+  <div p4>
     <div
       v-for="item in words" :key="item.word"
-      m-1 inline-block h-full w-auto gap-0 border rounded-xl p-1 dark:border-gray-7 bg-base
+      m-1 mb-2 inline-block h-full w-auto gap-0 border rounded-xl p-1 dark:border-gray-7 bg-base
     >
       <span block font-bold>{{ item.word }}</span> {{ item.meaning }}
     </div>
