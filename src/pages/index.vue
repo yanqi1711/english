737c84157323c9a042ddddcf62c61db1c6022742
yanqi <hi@yanqi.me>
2025-07-10 15:05:07 +0800

    chore: google translate

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,12 +5,17 @@ const words = ref([])
 
 async function loadWords() {
   try {
-    const response = await fetch('/data.json') // 直接加载 JSON
+    const response = await fetch('/data.json')
     words.value = await response.json()
   }
   catch (error) {
     console.error('Error loading data:', error)
   }
+}
+
+function goToPage(word) {
+  const link = `https://translate.google.com/?sl=en&tl=zh-CN&text=${word}&op=translate`
+  window.open(link, '_blank')
 }
 
 onMounted(loadWords)
@@ -20,9 +25,13 @@ onMounted(loadWords)
   <div p4>
     <div
       v-for="item in words" :key="item.word"
-      m-1 mb-2 inline-block h-full w-auto gap-0 border rounded-xl p-1 dark:border-gray-7 bg-base
+      class="card"
+      m-1
+      mb-2 inline-block h-full w-auto gap-0 border rounded-xl p-1 dark:border-gray-7 bg-base
     >
-      <span block font-bold>{{ item.word }}</span> {{ item.meaning }}
+      <div cursor-pointer @click="goToPage(item.word)">
+        <a>{{ item.word }}</a><br>{{ item.meaning }}
+      </div>
     </div>
   </div>
 </template>
